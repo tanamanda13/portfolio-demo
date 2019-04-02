2345395a4148d03a6bc0ea6713c4071aba6a626e
tanamanda13 <tanamanda13@gmail.com>
2019-04-02 15:32:36 +0200

    Fix, aboutme

---
+++ src/js/scripts.js
@@ -0,0 +1,6 @@
+//alert('hello');
+
+document.querySelector('.aboutMe__photo').addEventListener('click', function(){
+  console.log('ok');
+  document.querySelector('.aboutMe__text').classList.toggle('aboutMe__text--show');
+})
