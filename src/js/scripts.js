a5519895aac93b0e728baabee31524c577ae327b
tanamanda13 <tanamanda13@gmail.com>
2019-04-03 10:50:47 +0200

    Projects init

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,4 +3,5 @@
 document.querySelector('.aboutMe__photo').addEventListener('click', function(){
   console.log('ok');
   document.querySelector('.aboutMe__text').classList.toggle('aboutMe__text--show');
+  document.documentElement.scrollTop = 454;
 })
