c8c63e8626d1b06daccb369b260d3ec538f12f93
tanamanda13 <tanamanda13@gmail.com>
2019-04-03 11:55:30 +0200

    aboutme close

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,18 @@
 //alert('hello');
 
+// document.querySelector('.aboutMe__photo').addEventListener('click', function(){
+//   console.log('ok');
+//   document.querySelector('.aboutMe__text').classList.toggle('aboutMe__text--show');
+//   document.documentElement.scrollTop = 454;
+// })
+
 document.querySelector('.aboutMe__photo').addEventListener('click', function(){
   console.log('ok');
-  document.querySelector('.aboutMe__text').classList.toggle('aboutMe__text--show');
+  document.querySelector('.aboutMe__text').classList.add('aboutMe__text--show');
   document.documentElement.scrollTop = 454;
+})
+
+document.querySelector('.aboutMe__text--close').addEventListener('click', function(){
+  console.log('ko');
+  document.querySelector('.aboutMe__text').classList.remove('aboutMe__text--show');
 })
