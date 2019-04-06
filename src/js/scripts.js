1b4101effb656d460a3b21966cf607611fe4e267
tanamanda13 <tanamanda13@gmail.com>
2019-04-06 18:48:50 +0200

    finished desktop 1st version

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,7 +9,7 @@
 document.querySelector('.aboutMe__photo').addEventListener('click', function(){
   console.log('ok');
   document.querySelector('.aboutMe__text').classList.add('aboutMe__text--show');
-  document.documentElement.scrollTop = 454;
+  document.documentElement.scrollTop = 485;
 })
 
 document.querySelector('.aboutMe__text--close').addEventListener('click', function(){
